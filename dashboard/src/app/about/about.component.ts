9832b12f32cbbe3bef9b54c2e6d986fe2e459220
binnie <lananh4415@gmail.com>
2023-10-26 10:07:05 +0700

    created layout component

---
+++ dashboard/src/app/about/about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.less'],
+})
+export class AboutComponent {}
