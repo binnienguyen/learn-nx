c2a2354c631de2abbfdc41b12a9a8e4e5ab681da
vietpham <ducviet169614@gmail.com>
2023-11-05 03:54:09 +0700

    create layout component

--- dashboard/src/app/remote-entry/entry.module.ts
+++ dashboard/src/app/remote-entry/entry.module.ts
@@ -15,11 +15,20 @@ import {NzMenuModule} from "ng-zorro-antd/menu";
 import {NzDropDownModule} from "ng-zorro-antd/dropdown";
 import {NzEmptyModule} from "ng-zorro-antd/empty";
 import {NzPageHeaderModule} from "ng-zorro-antd/page-header";
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import {FormsModule} from "@angular/forms";
+import {SelectComponent} from "./select.component";
+import {NzOverlayModule} from "ng-zorro-antd/core/overlay";
+import {OverlayModule} from "@angular/cdk/overlay";
+import {NzGridModule} from "ng-zorro-antd/grid";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzSpaceModule} from "ng-zorro-antd/space";
+import {NzButtonModule} from "ng-zorro-antd/button";
 
 @NgModule({
-  declarations: [RemoteEntryComponent, NxWelcomeComponent],
-  imports: [CommonModule, RouterModule.forChild(remoteRoutes), NzLayoutModule, NzBreadCrumbModule,
-    NzDividerModule, NzTableModule, NzAvatarModule, NzIconModule, NzMenuModule, NzDropDownModule, NzEmptyModule, NzPageHeaderModule],
-  providers: [],
+  declarations: [RemoteEntryComponent, NxWelcomeComponent, SelectComponent],
+  imports: [CommonModule, NzOverlayModule, RouterModule.forChild(remoteRoutes), NzLayoutModule, NzBreadCrumbModule,
+    NzDividerModule, NzTableModule, NzAvatarModule, NzIconModule, NzMenuModule, NzDropDownModule, NzEmptyModule, NzPageHeaderModule, NzSelectModule, FormsModule, OverlayModule, NzGridModule, NzInputModule, NzSpaceModule, NzButtonModule],
+  providers: [OverlayModule],
 })
 export class RemoteEntryModule {}
--- dashboard/src/app/remote-entry/nx-welcome.component.ts
+++ dashboard/src/app/remote-entry/nx-welcome.component.ts
@@ -1,24 +1,44 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-interface Person {
-  key: string;
+interface Security {
+  id: number;
   name: string;
-  age: number;
-  address: string;
+  desc: string;
+}
+
+interface Inbound {
+  ip_version: string;
+  protocol: string;
+  port_range: string;
+  remote: string;
 }
 
 @Component({
   selector: 'my-app-nx-welcome',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.less'],
-  // encapsulation: ViewEncapsulation.None,
 })
 
 export class NxWelcomeComponent implements OnInit{
-  // isCollapsed = false;
 
-  // constructor(private userService: UserService) {}
-  isCollapsed = false;
+  selectedValue?: Security;
+  options: Security[] = [
+    { name: 'Security 1', id: 1, desc: '' },
+    { name: 'Security 2', id: 2, desc:''},
+    { name: 'Security 3', id: 3, desc:"" },
+  ];
 
+  listInbound: Inbound[] = [
+    { ip_version: 'ipV4', protocol: 'tcp', port_range: '1', remote: 'Test 1'},
+    { ip_version: 'ipV4', protocol: 'tcp', port_range: '2', remote: 'Test 1'},
+    { ip_version: 'ipV4', protocol: 'tcp', port_range: '2', remote: 'Test 1'},
+    { ip_version: 'ipV4', protocol: 'tcp', port_range: '3', remote: 'Test 1'},
+    { ip_version: 'ipV4', protocol: 'tcp', port_range: '3', remote: 'Test 1'},
+  ]
+  onSelectChange(): void {
+    console.log('Selected value: ' + this.selectedValue);
+  }
+
+  inputValue?: string
   ngOnInit() {}
 }
